e3baa9c1ecac5d5a59c0ff069117b19797ae79dc
Ashley Ho <ashley.demei.ho@gmail.com>
2018-04-26 16:34:02 -0700

    started time validations (incomplete) linked to Event Details. Linking to Locations

--- Models/VolunteerModels/Event.cs
+++ Models/VolunteerModels/Event.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using C_Sharp_Project.Models;
 
@@ -7,11 +8,23 @@ namespace VolunteerPlanner.Models
 {
     public class Event : BaseEntity
     {
+        [Key]
         public int EventId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage="Name must contain at least 2 characters!")]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime EndDate { get; set; }
+
+        [Required]
         public string Address { get; set; }
         public int CoordinatorId { get; set; }
         public User Coordinator { get; set; }
@@ -42,6 +55,47 @@ namespace VolunteerPlanner.Models
             }
             return joined;
         }
+
+        public bool CanJoin(User user)
+        {
+            bool canJoin = true;
+
+            foreach(var joinedEvent in user.Events)
+            {
+                DateTime joinedEndDate = joinedEvent.Event.EndDate;
+                DateTime joinedStartDate = joinedEvent.Event.StartDate;
+
+                if((StartDate > joinedStartDate && StartDate < joinedEndDate) || (EndDate > joinedStartDate && EndDate < joinedEndDate) || (StartDate < joinedStartDate && EndDate > joinedEndDate))
+                {
+                    canJoin = false;
+                    break;
+                }
+            }
+            return canJoin;
+        }
+
+        public TimeSpan GetDuration(DateTime StartDate, DateTime EndDate)
+        {
+            TimeSpan duration = StartDate.Subtract(EndDate);
+            return duration;
+        }
+
+        // public class CurrentDateAttribute : ValidationAttribute
+        // {
+        //     public CurrentDateAttribute()
+        //     {
+        //     }
+
+        //     public override bool IsValid(object value)
+        //     {
+        //         var dt = (DateTime)value;
+        //         if(dt > DateTime.Now)
+        //         {
+        //             return true;
+        //         }
+        //         return false;
+        //     }
+        // }
         
     }
 }
